4c4ab1018d701a68d57d0dca4639de0e61a3f33f
xTheCoderx <justaguyoney@gmail.com>
2021-10-18 11:28:08 +1300

    Configured GameFragment.GameFragmentListener.

--- app/src/main/java/com/realtomjoney/memorygame/GameFragment.kt
+++ app/src/main/java/com/realtomjoney/memorygame/GameFragment.kt
@@ -1,14 +1,31 @@
 package com.realtomjoney.memorygame
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class GameFragment : Fragment() {
+    interface GameFragmentListener {
+        fun makeTiles(): ArrayList<TextView>
+    }
+
+    private lateinit var caller: GameFragmentListener
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context is GameFragmentListener) {
+            caller = context
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,6 +34,12 @@ class GameFragment : Fragment() {
             R.layout.fragment_game,
             container,
             false)
+
+        val context = activity as Context
+        val recyclerView: RecyclerView = fragment.findViewById(R.id.gameRecyclerView)
+        recyclerView.layoutManager = GridLayoutManager(context, 4)
+        val textViews = caller.makeTiles()
+        recyclerView.adapter = GameRecyclerAdapter(textViews)
 
         return fragment
     }
--- app/src/main/java/com/realtomjoney/memorygame/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/memorygame/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.realtomjoney.memorygame
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GameFragment.GameFragmentListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,5 +14,18 @@ class MainActivity : AppCompatActivity() {
             .beginTransaction()
             .add(R.id.gameLayout, GameFragment.newInstance(),
             "game").commit()
+    }
+
+    override fun makeTiles(): ArrayList<TextView> {
+        val tilesArray: ArrayList<TextView> = ArrayList()
+        for (i in 1..16) {
+            val newTile = TextView(this)
+            newTile.text = "Hi"
+            newTile.setTextColor(Color.BLACK)
+            newTile.setBackgroundColor(Color.RED)
+
+            tilesArray.add(newTile)
+        }
+        return tilesArray
     }
 }
