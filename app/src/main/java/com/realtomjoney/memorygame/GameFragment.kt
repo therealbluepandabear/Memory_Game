6993c5e0837b50c4d8d7f40027525361d3b80f86
xTheCoderx <justaguyoney@gmail.com>
2021-10-18 10:32:29 +1300

    Added fragment_game.xml, added GameRecyclerAdapter, added RecylerViewHolder.

--- app/src/main/java/com/realtomjoney/memorygame/GameFragment.kt
+++ app/src/main/java/com/realtomjoney/memorygame/GameFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class GameFragment : Fragment() {
     override fun onCreateView(
@@ -24,4 +26,26 @@ class GameFragment : Fragment() {
             return GameFragment()
         }
     }
+}
+class RecyclerViewHolder(inflater: LayoutInflater, parent: ViewGroup) : RecyclerView.ViewHolder(inflater.inflate(R.layout.card_layout, parent, false)) {
+    val tileParent: SquareFrameLayout = itemView.findViewById(R.id.tileParent)
+}
+
+class GameRecyclerAdapter(private val inputData: ArrayList<TextView>) : RecyclerView.Adapter<RecyclerViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return RecyclerViewHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+        val thisTile = inputData[position]
+
+        holder.tileParent.addView(thisTile)
+    }
+
+    override fun getItemCount() = inputData.size;
+
 }
