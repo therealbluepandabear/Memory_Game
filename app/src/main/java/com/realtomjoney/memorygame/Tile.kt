88622ef28d921aa294a69578b0e66607cc248c01
xTheCoderx <justaguyoney@gmail.com>
2021-10-19 14:00:31 +1300

    Added flipping animation to cards.

--- app/src/main/java/com/realtomjoney/memorygame/Tile.kt
+++ app/src/main/java/com/realtomjoney/memorygame/Tile.kt
@@ -1,7 +1,12 @@
 package com.realtomjoney.memorygame
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.graphics.Color
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
 import android.widget.TextView
 
 enum class Status {
@@ -19,19 +24,38 @@ data class Tile(var myContext: Context, var value: Int) : androidx.appcompat.wid
     var tileStatus: Status = Status.UNKNOWN
 
     fun updateTile() {
-        when (tileStatus) {
-            Status.UNKNOWN -> {
-                this@Tile.text = "?"
-                this@Tile.setBackgroundColor(Color.DKGRAY)
+        val objectAnimator1 = ObjectAnimator.ofFloat(this, "scaleX", 1f, 0f)
+        val objectAnimator2 = ObjectAnimator.ofFloat(this, "scaleX", 0f, 1f)
+
+        objectAnimator1.duration = 250
+        objectAnimator2.duration = 250
+
+        objectAnimator1.interpolator = DecelerateInterpolator()
+        objectAnimator2.interpolator = AccelerateInterpolator()
+
+        objectAnimator1.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                super.onAnimationEnd(animation)
+
+                objectAnimator2.start()
+
+                when (tileStatus) {
+                    Status.UNKNOWN -> {
+                        this@Tile.text = "?"
+                        this@Tile.setBackgroundColor(Color.DKGRAY)
+                    }
+                    Status.FLIPPED -> {
+                        this@Tile.text = value.toString()
+                        this@Tile.setBackgroundColor(Color.YELLOW)
+                    }
+                    Status.FOUND -> {
+                        this@Tile.text = "😊"
+                        this@Tile.setBackgroundColor(Color.GREEN)
+                    }
+                }
             }
-            Status.FLIPPED -> {
-                this@Tile.text = value.toString()
-                this@Tile.setBackgroundColor(Color.YELLOW)
-            }
-            Status.FOUND -> {
-                this@Tile.text = "😊"
-                this@Tile.setBackgroundColor(Color.GREEN)
-            }
-        }
+        })
+
+        objectAnimator1.start()
     }
 }
